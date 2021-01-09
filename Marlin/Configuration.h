75a2e5f6d74d4685312468284dcb4e25800d8dcd
lococnc <46273940+lococnc@users.noreply.github.com>
2021-01-09 15:36:06 -0600

    new

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -72,7 +72,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(mgamber, Paula's Ender 3 Pro)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(mgamber, Paula's Ender 3 Pro w/ SKR E3 Turbo)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
